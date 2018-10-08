bbdad760b050bdb16ae5942007e167b4258f1bbd
Esben Frihauge <esj@frihauge.dk>
2018-10-08 07:06:14 +0200

    526

--- pillrobot_Test/pillrobot_Test.ino
+++ pillrobot_Test/pillrobot_Test.ino
@@ -15,20 +15,23 @@
 Servo Serv_PillRelase;  // create servo object to control a servo
 Servo Serv_Pillfeed;  // create servo object to control a servo
 
-enum State_enum {STOP, PILLFEED, PILLRELASE, VIBRATE};
-enum Sensors_enum {NONE, PILLFEED, SENSOR_LEFT, BOTH};
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte PillFeed = LOW;
+
+enum State_enum {STOP, FAIL, PILLFEED, PILLREALSE, VIBRATE};
+enum Sensors_enum {NONE, PILLFEED_DET, SENSOR_LEFT, BOTH};
 
  
 void state_machine_run(uint8_t sensors);
-void motors_stop();
-void onepillfeed();
-void motors_right();
-void motors_left();
-uint8_t read_IR();
- 
-uint8_t state = STOP;
+bool onepillfeed();
+//void vibrate();
 
-uint16_t PillOK_Count[9] = 0; //Pill release after 0 - 8 vibration 
+
+uint8_t state = STOP;
+ 
+uint8_t VibrateCount = 0;
+uint16_t PillOK_Count[9] = {0}; //Pill release after 0 - 8 vibration 
 
  
 
@@ -37,7 +40,9 @@ void setup() {
   Serial.begin(9600);
   Serv_PillRelase.attach(9);  // attaches the servo on pin 9 to the servo object
   Serv_Pillfeed.attach(8);  // attaches the servo on pin 9 to the servo object
-  
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), PillFeedDet, CHANGE); 
   
   //pinMode(ADC_CHANNEL0_PIN, INPUT);
   //pinMode(GPIO_PORT0_PIN, INPUT);
@@ -50,10 +55,9 @@ void loop(){
 
   if(Serial.available()> 0)
   {
-    process_serial(Serial.read(),&STATE,TEXT_BUEFFER,&TEXT_BUEFFER_CNT);
   }
  
-  state_machine_run(read_sensor());
+  state_machine_run(PILLFEED);
  
   delay(10);
 }
@@ -68,45 +72,65 @@ void state_machine_run(uint8_t sensors)
        state = STOP;
       }
       else if(sensors == PILLFEED){
-        onepillfeed();
+       
         state = PILLFEED;
       }
-      else{
-        motors_right();
-        state = ROTATE_RIGHT;
-      }
       break;
-       
+    case FAIL:
+      break;   
     case PILLFEED:
-      if(sensors != PILLDEEDED){
-        vibrate();
+      bool stat = onepillfeed();
+      
+      if(!stat){
         state = VIBRATE;
       }
       break;
     case PILLREALSE:
-      if(sensors != PILLDEEDED){
-        vibrate();
+      if(sensors != PILLFEED_DET){
         state = VIBRATE;
       }
       break;
     case VIBRATE:
-      if(sensors != SENSOR_RIGHT)
-      {
-        motors_stop();
-        state = STOP; 
-      }
+      vibrate();
+      VibrateCount++;
+      if (VibrateCount <= 8)
+        state = PILLFEED;
+      else
+        state = FAIL;
       break;
   }
 }
  
 
-void onepillfeed()
+bool onepillfeed()
 {
-  //code for driving forward  
+ uint8_t pos = 0;
+  bool stat = false;
+  //code for feedig one pill
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Serv_Pillfeed.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }  
+
+  for (pos = 180; pos >= 0; pos -= 1) { 
+    Serv_Pillfeed.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  delay(100);
+  stat = PillFeed; 
+  if(PillFeed == true)
+  { 
+    PillFeed = false;
+  }
+    
+  return  stat; 
 }
 
- 
-uint8_t read_IR()
+void PillFeedDet() {
+  PillFeed = true;
+}
+uint8_t vibrate()
 {
-  //code for reading both sensors
+ 
 }
